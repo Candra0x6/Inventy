90acdd45ec3a057f2fd2fabba10ed8996be51f53
Candra0x6 <kurniawancandram@gmail.com>
2025-10-14 13:57:35 +0700

    fix: update session token cookie domain for production environment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -76,7 +76,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         secure: process.env.NODE_ENV === "production",
-        domain: process.env.NODE_ENV === "production" ? ".vercel.app" : undefined,
+        domain: process.env.NODE_ENV === "production" ? "inventy-seven.vercel.app" : undefined,
       },
     },
   },
